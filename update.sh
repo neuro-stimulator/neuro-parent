c3dc567443343721d58314269009d4bfebb1b390
petr <stechy12@gmail.com>
2020-04-11 16:04:31 +0200

    Aktualizace skriptu pro instalaci aplikace

--- update.sh
+++ update.sh
@@ -36,11 +36,16 @@ echo 'kopiruji package.json'
 cp diplomka-backend/package.json application
 echo 'kopiruji package-lock.json'
 cp diplomka-backend/package-lock.json application
-echo 'kopiruji .env soubor'
+echo 'kopiruji .env.production soubor'
 cp diplomka-backend/.env.production application
+echo 'prejmenovavam .env.production na .env'
 mv application/.env.production application/.env
+echo 'vytvarim zdrojovou slozku serveru'
+mkdir application/src
 echo 'kopiruji server do slozky application...'
-cp -R diplomka-backend/dist/* application
+cp -R diplomka-backend/dist/* application/src
+echo 'vytvarim slozku pro schemata'
+mkdir application/schemas
 cp -R diplomka-backend/schemas/*.json application/schemas
 cp -R diplomka-backend/triggers application
 echo 'kopiruji zavislosti serveru'
